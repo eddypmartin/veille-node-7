2d2cee422140e2507e7d800a545eb9916ef57ece
Eddy Martin <eddypmartin@gmail.com>
2018-02-20 18:34:18 -0500

    Départ de l'exercice #7

---
+++ mes_modules/peupler/index.js
@@ -0,0 +1,21 @@
+"use strict"
+const tableau  = require('./tableaux.js')
+
+const max = tableau.length
+
+console.log('max= ' + max)
+
+const peupler_json = () => {
+
+  let position 
+  let tabVille = []
+  for (let k=0 ; k<10; k++)
+	{
+		position = Math.floor(Math.random()*max)
+		tabVille.push(tableau[position])
+	}
+  return(tabVille)
+}
+
+
+module.exports = peupler_json
---
+++ mes_modules/peupler/tableaux.js
@@ -0,0 +1,14 @@
+"use strict";
+let tableau = [
+				"Montréal",
+				"Paris",
+				"Québec",
+				"Laval",
+				"Bruxelle",
+				"Ottawa",
+				"Calgary",
+				"Gaspé",
+				"Gatineau"
+				];
+
+module.exports = tableau;				
